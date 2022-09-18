e0918739f9da9a37b9929f0781984b7f80f09d63
amandaling32 <72606729+amandaling32@users.noreply.github.com>
2022-09-19 05:14:45 +0800

    changes

--- detect.py
+++ detect.py
@@ -57,7 +57,7 @@ def run(
         max_det=1000,  # maximum detections per image
         device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
         view_img=False,  # show results
-        save_txt=False,  # save results to *.txt
+        save_txt=True,  # save results to *.txt
         save_conf=False,  # save confidences in --save-txt labels
         save_crop=False,  # save cropped prediction boxes
         nosave=False,  # do not save images/videos
@@ -220,7 +220,7 @@ def parse_opt():
     parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--view-img', action='store_true', help='show results')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-txt', action='store_true', default='true', help='save results to *.txt')
     parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
     parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
     parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
---
+++ saveResults.py
@@ -0,0 +1,31 @@
+import os
+
+# Folder to copy from
+directory = './runs/detect/exp15/labels'
+
+# Open a new file to write to
+output_file = open('output.txt','w+')
+
+# This starts the 'walk' through the directory
+for folder , sub_folders , files in os.walk(directory):
+
+    # For each file...
+    for f in files:
+        # create the current path using the folder variable plus the file variable
+        current_path = folder+"\\"+f
+
+        #  
+
+        # Open the file to read the contents
+        current_file = open(current_path, 'r')
+
+        # read each line one at a time and then write them to your file
+        for line in current_file:
+            output_file.write(line[:2])
+            output_file.write("\n")
+
+        # close the file
+        current_file.close()
+
+#close your output file
+output_file.close()
